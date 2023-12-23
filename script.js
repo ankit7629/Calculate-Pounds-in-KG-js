b03df09e1a9a95529185b6e2b73e60ba294a31de
ankit7629 <ankitthakur7629@gmail.com>
2023-12-23 14:50:26 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// const input = document.getElementById('input');
+// const submitBtn = document.getElementById('submit');
+// const result = document.getElementById('result');
+
+// submitBtn.addEventListener('click', (e) => {
+//     console.log(e.target.value,input.value)
+
+//     if(input.value === ""){
+//        alert('enter the value')
+//     }else{
+//         let weight = input.value/2.205;
+//         let total = weight;
+//         result.innerHTML = `Your Weight in KG: ${total.toFixed(2)}`
+//     }
+// });
+
+// function calculateWeight() {
+//     const input = document.getElementById('input');
+    
+//     if(input.value === ''){
+//         alert('enter the value')
+//     };
+     
+//     let weightinPound = input.value/2.205;
+//     let total = weightinPound
+//     document.getElementById('result').innerHTML = `Your Weight in <span>KG</span> ${total.toFixed(2)}`
+// }
+
+// document.getElementById('submit').onclick = () => {
+//     calculateWeight()
+// }
